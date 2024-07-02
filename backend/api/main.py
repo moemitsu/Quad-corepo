7dec166b6cd34aa3029732f5c9efb91b628d9d4e
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-02 09:42:58 +0900

    メソッドの@app～のみ追加しました。

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,7 +5,25 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
+# ログイン
+@app.post("/api/v1/auth/login")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+# ユーザー情報登録
+@app.post("/api/v1/user")
+
+# ユーザー情報編集
+@app.put("/api/v1/user/{user_id}")
+
+# 子どもの追加
+@app.post("/api/v1/user/{user_id}/children")
+
+# 各月画面の情報を取得
+@app.get("/api/v1/main")
+
+# 記録の追加
+@app.post("/api/v1/records")
+
+# LLM分析
+@app.post("/api/v1/analysis")
