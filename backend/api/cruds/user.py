672cf4cee84d757d541ec487aa7f1c9d9eb9dc31
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-04 12:58:27 +0900

    ユーザー登録用の関数を追加しました。

--- backend/api/cruds/user.py
+++ backend/api/cruds/user.py
@@ -12,3 +12,11 @@ def getStakeholder(db: Session, stakeholder_name: str):
 # Userテーブルから子どもの名前を取得
 def getChild(db: Session, child_name: str):
   return db.query(models.User).filter(models.User.child_name == child_name).first
+
+# ユーザーを登録
+def createUser(db: Session, user: schemas.UserReq):
+  dbUser = models.User(user_name = user.user_name)
+  db.add(dbUser)
+  db.commit()
+  db.refresh(dbUser)
+  return dbUser
