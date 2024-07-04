0fb141da0f8bd22cfa32d31816d854685df83aa9
Miyuki-seki <ushikichi@gmail.com>
2024-07-04 11:17:23 +0900

    backend/マイグレーションまでDocker内でできました。また、main.pyにcors設定を入れてみました。

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI, HTTPException, Depends, Path, Query, Body
+from api.lib.auth import get_current_user
 from pydantic import BaseModel
 from typing import List, Optional, Dict, Any
-
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime, timedelta
 
 # FastAPIをインスタンス化する
 app = FastAPI()
 
+origins = [
+    "http://localhost:8000",
+    "https://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # 初期のやつ
 @app.get("/")
-async def index():
-  return {"message": "Hello World"}
+def read_root():
+  return {"message": "Welcome to the FastAPI application"}
+
+@app.get("/protected-route")
+def protected_route(user = Depends(get_current_user())):
+  return {"message": "This is a protected route", "user": user}
