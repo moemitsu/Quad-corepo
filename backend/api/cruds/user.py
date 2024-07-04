e1f0ec19128ed03ab6edc30a93f10a09738d885d
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-04 09:31:03 +0900

    利用者の名前取得・子供の名前取得のCRUD定義を追加しました

--- backend/api/cruds/user.py
+++ backend/api/cruds/user.py
@@ -1,6 +1,14 @@
 from sqlalchemy.orm import Session
-import models, backend.api.schemas.schemas as schemas
+import backend.api.database.models as models, backend.api.schemas.schemas as schemas
 
 # Userテーブルのidを取得
 def getUserId(db: Session, id: int):
   return db.query(models.User).filter(models.User.id == id).first()
+
+# Userテーブルから利用者の名前を取得
+def getStakeholder(db: Session, stakeholder_name: str):
+  return db.query(models.User).filter(models.User.stakeholder_name == stakeholder_name).first
+
+# Userテーブルから子どもの名前を取得
+def getChild(db: Session, child_name: str):
+  return db.query(models.User).filter(models.User.child_name == child_name).first
