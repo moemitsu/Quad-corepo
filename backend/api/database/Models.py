6cae996f74a11cc37ba27ce5c33118cf7022dc44
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-04 15:26:46 +0900

    データベース設計に合わせて修正しました。

--- backend/api/database/Models.py
+++ backend/api/database/Models.py
@@ -1,28 +1,38 @@
 from sqlalchemy import String, Column, Integer, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from backend.api.database.db import Base
 from uuid import uuid4
 
 # データモデルの作成
 # sqlalchemyによってデータベースのテーブルが生成
 
-class User(Base):
-  __tablename__ = 'User'
+class Stakeholder(Base):
+  __tablename__ = 'Stakeholder'
   id = Column(Integer, UUID(), nullable=False, primary_key=True, autoincrement= True)  # intで連番で採番される
-  user_name = Column(String(), nullable=False)
+  stakeholder_name = Column(String(), nullable=False)
+  firebase_id = Column(String(), nullable=False)
 
 class Payments(Base):
   __tablename__ = 'Payments'
   id = Column(Integer, UUID(), nullable=False, primary_key=True, autoincrement= True)  # intで連番で採番される
-  user_id = Column(UUID(), ForeignKey('user.id'), nullable=False, primary_key=True)
+  stakeholder_id = Column(Integer, ForeignKey('Stakeholder.id'), nullable=False, primary_key=True)
+  user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+
+class User(Base):
+  __tablename__ = 'User'
+  id = Column(Integer, UUID(), nullable=False, primary_key=True, autoincrement= True)  # intで連番で採番される
+  stakeholder_id = Column(Integer, ForeignKey('Stakeholder.id'), nullable=False)
+  adult_name = Column(String(), nullable=False)
+  child_name = Column(String(), nullable=False)
 
 class TimeShareRecords(Base):
   __tablename__ = 'TimeShareRecords'
   id = Column(Integer, UUID(), nullable=False, primary_key=True, autoincrement= True)  # intで連番で採番される
-  user_id = Column(UUID(), ForeignKey('user.id'), nullable=False, primary_key=True)
-  with_member = Column(String(), nullable=False)
-  child_name = Column(String(), nullable=False)
+  stakeholder_id = Column(Integer, ForeignKey('Stakeholder.id'), nullable=False, primary_key=True)
+  with_member = Column(String(), ForeignKey('User.adult_name'), nullable=False)
+  child_name = Column(String(), ForeignKey('User.child_name'), nullable=False)
   events = Column(String(), nullable=False)
   child_condition = Column(String(), nullable=False)
   place = Column(String(), nullable=False)
