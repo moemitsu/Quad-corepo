a30115517b6292b1892e4b371ab0720ede6ac4fe
Miyuki-seki <ushikichi@gmail.com>
2024-07-05 17:38:39 +0900

    backend/各routerにverify_tokenを挿入

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException, Depends, Path, Query, Body
-from api.lib.auth import get_current_user
+from fastapi import FastAPI, HTTPException, Request, Depends, Response
 from fastapi.middleware.cors import CORSMiddleware
+
 
 # FastAPIをインスタンス化する
 app = FastAPI()
@@ -17,14 +17,15 @@ app.add_middleware(
   allow_methods=["*"],
   allow_headers=["GET", "POST", "PUT","DELETE"],
   # allow_headers=["Authorization", "Content-Type"]
-
 )
 
+
 @app.get("/")
-def read_root():
+async def read_root():
   return {"message": "Welcome to the FastAPI application"}
 
 @app.get("/protected-route")
-def protected_route(user = Depends(get_current_user)):
-  return {"message": "This is a protected route", "userid": user.uid}
+async def protected_route(request: Request):
+  user = request.state.user
+  return {"message": f"Hello, {user['name']}"}
 
