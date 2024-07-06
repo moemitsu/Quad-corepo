3b3b3598c23a98b958991de90662b0498518b40c
Miyuki-seki <ushikichi@gmail.com>
2024-07-06 18:24:48 +0900

    baskend/docker環境勉強中

--- backend/entp0.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-poetry run uvicorn api.main:app --host 0.0.0.0 --reload
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,26 @@
 #! usr/bin/bash
 set -e  # エラーが発生した場合にスクリプトを終了する
 
+
+poetry run uvicorn api.main:app --host 0.0.0.0 --reload  >> output.log
+log=$(grep -q "done" output.log ) 
+  echo "waiting starting app1"
+
+echo $log
+  echo "waiting starting app2"
+while [ -z $log ]
+do 
+  echo "waiting starting app"
+  sleep 0.5
+done
+
+# output.logをクリア
+> output.log
+
 # 初期マイグレーションの作成
 echo "Running: poetry run alembic revision --autogenerate -m 'Initial migration'"
 poetry run alembic revision --autogenerate -m "Initial migration" | tee output.log
+
 
 # 'done'の確認
 if grep -q "done" output.log; then
