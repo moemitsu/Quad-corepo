3f568d59d2f3e08340e0d16e225d34f044d56874
mayumiseo <hyperballad.tokyo@gmail.com>
2024-07-08 15:13:47 +0900

    login,family-registration、修正

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,17 +1,17 @@
-// src/app/login/page.tsx
-
 'use client';
 
 import React from 'react';
 import LoginForm from '../../_components/forms/LoginForm';
-import Header from "../../_components/layout/header";
-import Footer from "../../_components/layout/footer";
+import Header from '../../_components/layout/header';
+import Footer from '../../_components/layout/footer';
 
 const LoginPage: React.FC = () => {
   return (
     <div>
       <Header />
-      <LoginForm />
+      <div className="p-6 min-h-screen flex flex-col justify-center items-center">
+        <LoginForm />
+      </div>
       <Footer />
     </div>
   );
