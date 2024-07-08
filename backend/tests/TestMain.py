4afe423ee373feadadbe677b0a7818ef213ace45
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-08 13:27:21 +0900

    リファクタリングしました。

--- backend/tests/TestMain.py
+++ backend/tests/TestMain.py
@@ -3,5 +3,5 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from api.db import getDB, Base
+from api.db import get_db, Base
 from api.main import app
