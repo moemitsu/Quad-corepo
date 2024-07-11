d798dabae8e6db26ff93c636c16df1fd26e3244e
Yuuki Koyanagi <brackcat09@yahoo.co.jp>
2024-07-11 14:51:48 +0900

    タイトルを追加しました。

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <div>
+      <title>corepo -これぽ- </title>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Image
