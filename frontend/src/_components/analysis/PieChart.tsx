91fedf42d3951cf79f746d5d250563d53c43fbe3
moemitsu <ilmmag.mk22@gmail.com>
2024-07-11 09:21:49 +0900

    [fix]円グラフ・棒グラフのデータセット修正中

--- frontend/src/_components/analysis/PieChart.tsx
+++ frontend/src/_components/analysis/PieChart.tsx
@@ -1,31 +1,37 @@
-// src/_components/PieChart.tsx
 'use client'
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { PieChartData } from '../../types/index'; // パスを適切に修正してください
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface PieChartProps {
-  data: any;
+  data: PieChartData;
   options?: any;
 }
 
 const PieChart: React.FC<PieChartProps> = ({ data, options }) => {
-  if (!data || !data.datasets) return <div>Loading...</div>;
-
   const chartOptions = {
     ...options,
     plugins: {
       legend: {
         display: true,
-        position: 'bottom', // 凡例の位置を設定
+        position: 'bottom',
         labels: {
           font: {
-            size: 16, // 凡例のフォントサイズを設定
+            size: 16,
           },
         },
       },
+      tooltip: {
+        callbacks: {
+          label: function(tooltipItem: any) {
+            const value = tooltipItem.raw.toFixed(2);
+            return `${tooltipItem.label}: ${value}%`;
+          }
+        }
+      }
     },
   };
 
