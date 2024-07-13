b6baddcccd73ea69f5b41b8e4645932ae468dcb9
Miyuki-seki <ushikichi@gmail.com>
2024-07-13 19:08:25 +0900

    backend/halloWorldのテストが通った。

---
+++ backend/tests/test_main.py
@@ -0,0 +1,41 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from api.database.db import get_db, Base
+from api.main import app
+import starlette.status
+ASYNC_DB_URL = "postgresql+asyncpg://postgres:password@db:5432/sectionfdb"
+
+
+@pytest_asyncio.fixture
+async def async_client() -> AsyncClient:
+    # Async用のengineとsessionを作成
+    async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+    async_session = sessionmaker(
+        autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+    )
+
+    # テスト用にオンメモリのpostgresテーブルを初期化（関数ごとにリセット）
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    # DIを使ってFastAPIのDBの向き先をテスト用DBに変更
+    async def get_test_db():
+        async with async_session() as session:
+            yield session
+
+    app.dependency_overrides[get_db] = get_test_db
+
+    # テスト用に非同期HTTPクライアントを返却
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+    response = await async_client.get("/")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    # response_obj = response.json()
+    # assert response_obj[0]["id"] == 1
